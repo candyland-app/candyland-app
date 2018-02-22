2ddcc51a7f5f1f691cf415a5c89df265bc3085c1
georgebax <baxopoulos.george@gmail.com>
2018-02-22 22:40:53 +0200

    (totally tryhard and unnecessary)

---
+++ api/src/main/java/com/candyland/operations/StringConstants.java
@@ -0,0 +1,20 @@
+package com.candyland.operations;
+
+/**
+ * Some constants to make the code prettier.
+ */
+public class StringConstants {
+
+    public static final String COMMA = ",";
+
+    public static final String SPACE = " ";
+
+    public static final String EMPTY = "";
+
+    public static final String NEWLINE = "\n";
+
+    public static final String DOT = ".";
+
+    public static final String EXCL_MARK = "!";
+
+}
