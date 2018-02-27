974a01f79f5509198a8ba9fb6edd4c759237bc89
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 13:10:19 +0200

    User model class

---
+++ admin-portal/src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+	public id: number;
+	public username: string;
+	public firstName: string;
+	public lastName: string;
+	public address: string;
+	public postalCode: number;
+	public telephone: number;
+	public email: string;
+	public locked: boolean;
+	public picture: string;
+}
