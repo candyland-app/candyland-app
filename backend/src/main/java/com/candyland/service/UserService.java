dc198f99c7ee1ee393d25a340d62e7a09ebd09a7
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 17:40:23 +0200

    User service API

---
+++ backend/src/main/java/com/candyland/service/UserService.java
@@ -0,0 +1,19 @@
+package com.candyland.service;
+
+import java.util.List;
+import java.util.Set;
+
+import com.candyland.domain.User;
+import com.candyland.domain.security.UserRole;
+
+public interface UserService {
+	User save(User user);
+
+	User findById(Long id);
+
+	User findByEmail (String email);
+
+	User findByUsername(String username);
+
+	User createUser(User user, Set<UserRole> userRoles);
+}
