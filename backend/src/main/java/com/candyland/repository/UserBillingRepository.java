99b96355c8365f3562bccaddd6c7b685af32bbae
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 16:29:26 +0200

    Initializing users CRUD repo

--- backend/src/main/java/com/candyland/repository/UserBillingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.candyland.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.candyland.domain.BillingAddress;
-
-public interface UserBillingRepository extends CrudRepository<UserBilling, Long> {
-
-}
