ad0bf6363c636602610f09056a17405a716cd96c
georgebax <baxopoulos.george@gmail.com>
2018-02-27 22:06:02 +0200

    lord forgive me

--- backend/src/main/java/com/candyland/utility/MailConstructor.java
+++ backend/src/main/java/com/candyland/utility/MailConstructor.java
@@ -2,7 +2,12 @@ package com.candyland.utility;
 
 import java.util.Locale;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +16,6 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
-
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
@@ -21,39 +25,57 @@ import com.candyland.domain.User;
 @Component
 public class MailConstructor {
 
-	@Autowired
-	private Environment env;
+    @Autowired
+    private Environment env;
 
-	@Autowired
-	private TemplateEngine templateEngine;
+    @Autowired
+    private TemplateEngine templateEngine;
 
-	public SimpleMailMessage constructNewUserEmail(User user, String password) {
-		String message="\nUser Credentials." + "\nUsername:"+user.getUsername()+"\nPassword:"+password;
-		SimpleMailMessage email = new SimpleMailMessage();
-		email.setTo(user.getEmail());
-		email.setSubject("Candyland - New User");
-		email.setText(message);
-		email.setFrom(env.getProperty("support.email"));
-		return email;
-	}
+    /**
+     * the document's filename.
+     */
+    String filename;
 
-	public MimeMessagePreparator constructOrderConfirmationEmail (User user, Order order, Locale locale) {
-		Context context = new Context();
-		context.setVariable("order", order);
-		context.setVariable("user", user);
-		context.setVariable("orderItemList", order.getOrderItemList());
-		String text = templateEngine.process("orderConfirmationEmailTemplate", context);
+    /**
+     * path to the file.
+     */
+    String filePath;
 
-		MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
-			@Override
-			public void prepare(MimeMessage mimeMessage) throws Exception {
-				MimeMessageHelper email = new MimeMessageHelper(mimeMessage);
-				email.setTo(user.getEmail());
-				email.setSubject("Order Confirmation"+order.getId());
-				email.setText(text, true);
-				email.setFrom(new InternetAddress("yankun@gmail.com"));
-			}
-		};
-		return messagePreparator;
-	}
+    MimeBodyPart attachment = new MimeBodyPart();
+
+    public SimpleMailMessage constructNewUserEmail(User user, String password) throws MessagingException {
+        String message = "\nUser Credentials." + "\nUsername:" + user.getUsername() + "\nPassword:" + password;
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(user.getEmail());
+        email.setSubject("Candyland - New User");
+        email.setText(message);
+        email.setFrom(env.getProperty("support.email"));
+        DataSource source = new FileDataSource(filePath);
+        attachment.setDataHandler(new DataHandler(source));
+        attachment.setFileName("ticket1234.pdf");
+        // TODO change to multipart.
+        return email;
+    }
+
+    public MimeMessagePreparator constructOrderConfirmationEmail(User user, Order order, Locale locale) {
+        Context context = new Context();
+        context.setVariable("order", order);
+        context.setVariable("user", user);
+        context.setVariable("orderItemList", order.getOrderItemList());
+        String text = templateEngine.process("orderConfirmationEmailTemplate", context);
+
+        MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper email = new MimeMessageHelper(mimeMessage);
+                email.setTo(user.getEmail());
+                email.setSubject("Order Confirmation" + order.getId());
+                email.setText(text, true);
+                email.setFrom(new InternetAddress("yankun@gmail.com"));
+                String attachmentFilename = "order" + order.getId() + ".pdf";
+                email.addAttachment(attachmentFilename, new FileDataSource("tickets/" + attachmentFilename));
+            }
+        };
+        return messagePreparator;
+    }
 }
