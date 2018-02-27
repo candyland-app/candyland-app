758d75d97e6fd1b7e4b065bdd453edb532c096f2
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 16:05:59 +0200

    Initializing payments CRUD repo

---
+++ backend/src/main/java/com/candyland/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.candyland.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.candyland.domain.Payment;
+
+public interface PaymentRepository extends CrudRepository<Payment, Long>{
+
+}
