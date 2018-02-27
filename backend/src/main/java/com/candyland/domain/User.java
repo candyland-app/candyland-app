306c5273fffb50e73c024bcb0a2748ea40eeecc2
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 15:12:52 +0200

    User class implementation

---
+++ backend/src/main/java/com/candyland/domain/User.java
@@ -0,0 +1,181 @@
+package com.candyland.domain;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.candyland.domain.security.Authority;
+import com.candyland.domain.security.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class User implements UserDetails, Serializable{
+
+	private static final long serialVersionUID = 902783495L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="Id", nullable=false, updatable = false)
+	private Long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+    private String address;
+    private String email;
+	private String phone;
+	private boolean locked = false;
+
+	@OneToMany(mappedBy = "user", cascade=CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonIgnore
+	private Set<UserRole> userRoles = new HashSet<>();
+
+	@OneToMany(cascade=CascadeType.ALL, mappedBy = "user")
+	private List<UserPayment> userPaymentList;
+
+	@OneToOne(cascade=CascadeType.ALL, mappedBy = "user")
+	private Checkout checkout;
+
+	@OneToMany(mappedBy="user")
+	private List<Order> orderList;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+
+	@Override
+	public boolean isLocked() {
+		return locked;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	public List<UserPayment> getUserPaymentList() {
+		return userPaymentList;
+	}
+
+	public void setUserPaymentList(List<UserPayment> userPaymentList) {
+		this.userPaymentList = userPaymentList;
+	}
+
+	public Checkout getCheckout() {
+		return checkout;
+	}
+
+	public void setCheckout(Checkout checkout) {
+		this.checkout = checkout;
+	}
+
+	public List<Order> getOrderList() {
+		return orderList;
+	}
+
+	public void setOrderList(List<Order> orderList) {
+		this.orderList = orderList;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		userRoles.forEach(ur -> authorities.add(new Authority(ur.getRole().getName())));
+
+		return authorities;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// Auto-generated method
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// Auto-generated method
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// Auto-generated method
+		return true;
+	}
+
+}
