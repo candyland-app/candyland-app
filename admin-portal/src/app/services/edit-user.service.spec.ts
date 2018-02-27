45cdfd7b7ad42d4509aea499e459c39661f6fd6d
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 12:56:08 +0200

    User editing service

---
+++ admin-portal/src/app/services/edit-user.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { EditUserService } from './edit-user.service';
+
+describe('EditUserService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [EditUserService]
+    });
+  });
+
+  it('should ...', inject([EditUserService], (service: EditUserService) => {
+    expect(service).toBeTruthy();
+  }));
+});
---
+++ admin-portal/src/app/services/edit-user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {User} from '../models/user';
+
+@Injectable()
+export class EditUserService {
+
+  constructor(private http:Http) { }
+
+  sendUser(user:User) {
+  	let url = "http://localhost:8181/user/update";
+
+    let headers = new Headers ({
+      'Content-Type': 'application/json',
+      'x-auth-token' : localStorage.getItem('xAuthToken')
+    });
+
+    return this.http.post(url, JSON.stringify(user), {headers: headers});
+  }
+}
