c327e40ce378de520e7c9398370997a8da521cc3
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 17:20:24 +0200

    Event service API

---
+++ backend/src/main/java/com/candyland/service/EventService.java
@@ -0,0 +1,18 @@
+package com.candyland.service;
+
+import java.util.List;
+
+import com.candyland.domain.Event;
+
+public interface EventService {
+
+	List<Event> findAll();
+
+	Event findOne(Long id);
+
+	Event save(Event event);
+
+	List<Event> blurrySearch(String name);
+
+	void removeOne(Long id);
+}
