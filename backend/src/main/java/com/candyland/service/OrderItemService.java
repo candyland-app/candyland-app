4583938c91d202ff40b87b30f03c571b5f64716d
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 17:33:26 +0200

    Order item/ticket service API

---
+++ backend/src/main/java/com/candyland/service/OrderItemService.java
@@ -0,0 +1,24 @@
+package com.candyland.service;
+
+import java.util.List;
+
+import com.candyland.domain.Event;
+import com.candyland.domain.OrderItem;
+import com.candyland.domain.Checkout;
+import com.candyland.domain.User;
+
+public interface OrderItemService {
+	OrderItem findById(Long id);
+
+	OrderItem save(OrderItem orderItem);
+
+    List<OrderItem> findByOrder(Order order);
+
+	void removeOrderItem(OrderItem orderItem);
+
+	OrderItem updateOrderItem(OrderItem orderItem);
+
+    List<OrderItem> findByCheckout(Checkout checkout);
+
+	OrderItem addEventToOrderItem(Event event, User user, int quantity);
+}
