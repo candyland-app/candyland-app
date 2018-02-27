6782517dad7d5eb2a12273beab84e38a18aad5ef
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-27 13:02:34 +0200

    Login service

---
+++ admin-portal/src/app/services/login.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class LoginService {
+
+  constructor(private http: Http) { }
+
+  sendCredential(username: string, password: string) {
+  	let url = "http://localhost:8181/token";
+  	let encodedCredentials = btoa(username+":"+password);
+  	let basicHeader = "Basic "+encodedCredentials;
+  	let headers = new Headers ({
+  		'Content-Type' : 'application/x-www-form-urlencoded',
+  		'Authorization' : basicHeader
+  	});
+
+  	return this.http.get(url, {headers: headers});
+
+  }
+
+  checkSession() {
+    let url = "http://localhost:8181/checkSession";
+    
+    let headers = new Headers ({
+      'x-auth-token' : localStorage.getItem('xAuthToken')
+    });
+
+    return this.http.get(url, {headers: headers});
+  }
+
+  logout() {
+    let url = "http://localhost:8181/user/logout";
+    
+    let headers = new Headers ({
+      'x-auth-token' : localStorage.getItem('xAuthToken')
+    });
+
+    return this.http.post(url, '', {headers: headers});
+  }
+}
