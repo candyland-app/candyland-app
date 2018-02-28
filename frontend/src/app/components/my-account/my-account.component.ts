db7537a74de4ab4c366bb94e2f1cb47dee48f9f2
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-28 21:02:38 +0200

    User account component

---
+++ frontend/src/app/components/my-account/my-account.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { UserService } from '../../services/user.service';
+import { AppConst } from '../../constants/app-const';
+
+@Component({
+	selector: 'app-my-account',
+	templateUrl: './my-account.component.html',
+	styleUrls: ['./my-account.component.css']
+})
+export class MyAccountComponent implements OnInit {
+
+	private serverPath = AppConst.serverPath;
+	private loginError: boolean = false;
+	private loggedIn = false;
+	private credential = { 'username': '', 'password': '' };
+
+	private emailSent: boolean = false;
+	private usernameExists: boolean;
+	private emailExists: boolean;
+	private username: string;
+	private email: string;
+
+	private emailNotExists: boolean = false;
+	private forgetPasswordEmailSent: boolean;
+	private recoverEmail: string;
+
+	constructor(
+		private loginService: LoginService,
+		private userService: UserService,
+		private router: Router
+	) { }
+
+	onLogin() {
+		this.loginService.sendCredential(this.credential.username, this.credential.password).subscribe(
+			res => {
+				console.log(res);
+				localStorage.setItem("xAuthToken", res.json().token);
+				this.loggedIn = true;
+				location.reload();
+				this.router.navigate(['/home']);
+			},
+			error => {
+				this.loggedIn = false;
+				this.loginError = true;
+			}
+		);
+	}
+
+	onNewAccount() {
+		this.usernameExists = false;
+		this.emailExists = false;
+		this.emailSent = false;
+
+		this.userService.newUser(this.username, this.email).subscribe(
+			res => {
+				console.log(res);
+				this.emailSent = true;
+			},
+			error => {
+				console.log(error.text());
+				let errorMessage = error.text();
+				if (errorMessage === "usernameExists") this.usernameExists = true;
+				if (errorMessage === "emailExists") this.emailExists = true;
+			}
+		);
+	}
+
+	onForgetPassword() {
+		this.forgetPasswordEmailSent = false;
+		this.emailNotExists = false;
+
+		this.userService.retrievePassword(this.recoverEmail).subscribe(
+			res => {
+				console.log(res);
+				this.forgetPasswordEmailSent = true;
+			},
+			error => {
+				console.log(error.text());
+				let errorMessage = error.text();
+				if (errorMessage === "Email not found") this.emailNotExists = true;
+			}
+		);
+	}
+
+	ngOnInit() {
+		this.loginService.checkSession().subscribe(
+			res => {
+				this.loggedIn = true;
+			},
+			error => {
+				this.loggedIn = false;
+			}
+		);
+	}
+
+}
