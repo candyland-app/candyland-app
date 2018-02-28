10ba2a7b2f9904b34690374802dacc6f65821c68
Klaus Sinani <klauscfhq@protonmail.com>
2018-02-28 22:29:58 +0200

    Event component model

---
+++ frontend/src/app/models/event.ts
@@ -0,0 +1,15 @@
+export class Event {
+	public id: number;
+	public name: string;
+	public minAge: number;
+	public maxAge: number;
+	public endTime: string;
+	public address: string;
+	public active: boolean;
+	public endDate: string;
+	public category: string;
+	public startTime: string;
+	public startDate: string;
+	public ticketPrice: number;
+	public postalCode: number;
+}
