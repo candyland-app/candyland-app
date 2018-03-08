2e899cb399f42b5055c13ea692af08b7cea447fe
PhilOrfan <33034450+PhilOrfan@users.noreply.github.com>
2018-03-08 04:19:26 +0200

    Update faq.component.ts

--- frontend/src/app/components/faq/faq.component.ts
+++ frontend/src/app/components/faq/faq.component.ts
@@ -5,11 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent implements OnInit {
+export class FaqComponent  {
 
-  constructor() { }
 
-  ngOnInit() {
+    myFunction1(element, text) {
+      var x = document.getElementById("myDIV1");
+      if (x.style.display === "block") {
+        element.textContent='+';
+        x.style.display = "none";
+      } else {
+        element.textContent=text;
+        x.style.display = "block";
+      }
+    }
+
+  myFunction2(element, text) {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      element.textContent='+';
+    } else {
+      x.style.display = "block";
+      element.textContent=text;
+    }
+  }
+
+  myFunction3(element, text) {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      element.textContent='+';
+    } else {
+      x.style.display = "block";
+      element.textContent=text;
+    }
   }
 
 }
