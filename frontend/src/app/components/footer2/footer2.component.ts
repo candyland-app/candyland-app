4490d43214d0c9aecbd1eea16ce6903c072f2c37
PhilOrfan <33034450+PhilOrfan@users.noreply.github.com>
2018-03-08 13:47:54 +0200

    Added footer

---
+++ frontend/src/app/components/footer2/footer2.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'footer2',
+  templateUrl: './footer2.component.html',
+  styleUrls: ['./footer2.component.css']
+})
+
+export class Footer2Component { }
