072977ffc514bd31da85572f190075c71ed85244
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-09 22:20:05 +0200

    Minor interface tweak

--- provider-portal/src/typings.d.ts
+++ provider-portal/src/typings.d.ts
@@ -1,5 +1,6 @@
 /* SystemJS module definition */
-declare var module: NodeModule;
-interface NodeModule {
+declare var module: InterfaceNodeModule;
+
+interface InterfaceNodeModule {
     id: string;
 }
