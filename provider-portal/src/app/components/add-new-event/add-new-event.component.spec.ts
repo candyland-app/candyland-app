01175cbcd997d83ab0fa48f32e6e490e51a06476
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-09 15:57:11 +0200

    Linted & prettified typescript

--- provider-portal/src/app/components/add-new-event/add-new-event.component.spec.ts
+++ provider-portal/src/app/components/add-new-event/add-new-event.component.spec.ts
@@ -3,23 +3,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddNewEventComponent } from './add-new-event.component';
 
 describe('AddNewEventComponent', () => {
-  let component: AddNewEventComponent;
-  let fixture: ComponentFixture<AddNewEventComponent>;
+    let component: AddNewEventComponent;
+    let fixture: ComponentFixture<AddNewEventComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AddNewEventComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(
+        async(() => {
+            TestBed.configureTestingModule({
+                declarations: [AddNewEventComponent]
+            }).compileComponents();
+        })
+    );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddNewEventComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AddNewEventComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
