96ad1d85fa786dede57e2251f0c58c2b3e2fdaa6
tsikos7 <konsmitr@gmail.com>
2018-03-14 17:29:11 +0200

    Updated events-list component

--- frontend/src/app/components/events-list/events-list.component.ts
+++ frontend/src/app/components/events-list/events-list.component.ts
@@ -1,3 +1,4 @@
+import { Events } from '../../models/events';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsListComponent implements OnInit {
     constructor() {}
+  
+    private event: Events = new Events();
+    private eventsList: Events[] = [];
+    
 
-    ngOnInit() {}
+    ngOnInit() {
+      this.event.name = 'Bird Watching';
+      this.event.address = 'Karamanli Street 33, Pagkrati';
+      this.event.category = 'Sports';
+      this.eventsList.push(this.event);
+      this.event = new Events();
+      this.event.name = 'Nemo Movie';
+      this.event.address = 'Kolokotroni Street 12, Psychiko';
+      this.event.category = 'Cinema';
+      this.eventsList.push(this.event);
+      this.event = new Events();
+      this.event.name = 'Tom o Bagasas';
+      this.event.address = '25is Martiou 323, Nea Smyrni';
+      this.event.category = 'Theatre';
+      this.eventsList.push(this.event);
+
+    }
+  
+    createStaticEventList () {
+      //this.eventList[0].name = 'Aloha';
+      
+    }
 }
