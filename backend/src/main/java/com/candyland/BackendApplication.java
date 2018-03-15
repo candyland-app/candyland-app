b266254cf3e95b33b1d14fa864b8acd80c40fd71
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-15 18:16:52 +0200

    Added missing spring auto-configuration annotation

--- backend/src/main/java/com/candyland/BackendApplication.java
+++ backend/src/main/java/com/candyland/BackendApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
 
     @Autowired
