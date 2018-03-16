dfb17acadd78f586ff06094b6d29590a92e18115
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-16 15:11:00 +0200

    Added event list component

---
+++ provider-portal/src/app/components/event-list/event-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Event } from '../../models/event';
+import { GetEventListService } from '../../services/get-event-list.service';
+import { LoginService } from '../../services/login.service';
+
+@Component({
+    selector: 'app-event-list',
+    templateUrl: './event-list.component.html',
+    styleUrls: ['./event-list.component.css']
+})
+export class EventListComponent implements OnInit {
+    private selectedEvent: Event;
+    private checked: boolean;
+    private eventList: Event[];
+    private allChecked: boolean;
+    private removeEventList: Event[] = new Array();
+
+    constructor(
+        private getEventListService: GetEventListService,
+        private router: Router
+    ) {}
+
+    ngOnInit() {
+        this.getEventListService.getEventList().subscribe(
+            res => {
+                console.log(res.json());
+                this.eventList = res.json();
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+}
