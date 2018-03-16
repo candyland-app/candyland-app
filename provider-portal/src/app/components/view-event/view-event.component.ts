6c74dd0b47c888b7ff8184b7db42a99189844d90
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-16 17:15:26 +0200

    Added event viewing component

---
+++ provider-portal/src/app/components/view-event/view-event.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Event } from '../../models/event';
+import { GetEventService } from '../../services/get-event.service';
+
+@Component({
+    selector: 'app-view-event',
+    templateUrl: './view-event.component.html',
+    styleUrls: ['./view-event.component.css']
+})
+export class ViewEventComponent implements OnInit {
+    private event: Event = new Event();
+    private eventId: number;
+
+    constructor(
+        private getEventService: GetEventService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {}
+
+    ngOnInit() {
+        this.route.params.forEach((params: Params) => {
+            this.eventId = Number.parseInt(params['id']);
+        });
+
+        this.getEventService.getEvent(this.eventId).subscribe(
+            res => {
+                this.event = res.json();
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+}
