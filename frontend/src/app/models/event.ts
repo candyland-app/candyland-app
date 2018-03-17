22f26e4dc9b817c7da72b0461a4edcdef3ec6e04
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-17 21:46:20 +0200

    Added event class model

---
+++ frontend/src/app/models/event.ts
@@ -0,0 +1,17 @@
+export class Event {
+    public id: number;
+    public name: string;
+    public address: string;
+    public zipcode: string;
+    public startDate: string;
+    public endDate: string;
+    public startTime: string;
+    public endTime: number;
+    public category: string;
+    public minAge: number;
+    public maxAge: number;
+    public price: number;
+    public active: boolean;
+    public description: string;
+    public availableTickets: number;
+}
