80e90ac612a0b6e43425dd00c5f7c8c5564db4bf
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-17 21:49:07 +0200

    Added event filtering pipe

---
+++ frontend/src/app/components/event-list/data-filter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
+
+@Pipe({
+    name: 'dataFilter'
+})
+export class DataFilterPipe implements PipeTransform {
+    transform(array: any[], query: string): any {
+        if (query) {
+            return _.filter(array, row => row.description.indexOf(query) > -1);
+        }
+        return array;
+    }
+}
