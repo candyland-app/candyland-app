3e043644634384d4141941020107ef3118695b40
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-18 18:31:46 +0200

    Added shopping-cart entity services

---
+++ backend/src/main/java/com/candyland/service/ShoppingCartService.java
@@ -0,0 +1,9 @@
+package com.candyland.service;
+
+import com.candyland.domain.ShoppingCart;
+
+public interface ShoppingCartService {
+    ShoppingCart updateShoppingCart(ShoppingCart shoppingCart);
+
+    void clearShoppingCart(ShoppingCart shoppingCart);
+}
---
+++ backend/src/main/java/com/candyland/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,56 @@
+package com.candyland.service.impl;
+
+import com.candyland.domain.CartItem;
+import com.candyland.domain.ShoppingCart;
+import com.candyland.repository.ShoppingCartRepository;
+import com.candyland.service.CartItemService;
+import com.candyland.service.ShoppingCartService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Autowired
+    private ShoppingCartRepository shoppingCartRepository;
+
+    public ShoppingCart updateShoppingCart(ShoppingCart shoppingCart) {
+        BigDecimal cartTotal = new BigDecimal(0);
+
+        List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
+
+        for (CartItem cartItem : cartItemList) {
+            if (cartItem.getEvent().getAvailableTickets() > 0) {
+                cartItemService.updateCartItem(cartItem);
+                cartTotal = cartTotal.add(cartItem.getSubtotal());
+            }
+        }
+
+        shoppingCart.setGrandTotal(cartTotal);
+
+        shoppingCartRepository.save(shoppingCart);
+
+        return shoppingCart;
+    }
+
+    public void clearShoppingCart(ShoppingCart shoppingCart) {
+        List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
+
+        for (CartItem cartItem : cartItemList) {
+            cartItem.setShoppingCart(null);
+            cartItemService.save(cartItem);
+        }
+
+        shoppingCart.setGrandTotal(new BigDecimal(0));
+
+        shoppingCartRepository.save(shoppingCart);
+    }
+
+}
