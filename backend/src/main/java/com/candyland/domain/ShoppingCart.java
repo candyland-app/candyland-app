eff0e48e9ab88097b30a1ebf5d7d3109fe7b6084
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-18 18:21:42 +0200

    Added shopping-cart entity model

---
+++ backend/src/main/java/com/candyland/domain/ShoppingCart.java
@@ -0,0 +1,66 @@
+package com.candyland.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class ShoppingCart implements Serializable {
+    private static final long serialVersionUID = -891273432L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private BigDecimal GrandTotal;
+
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<CartItem> cartItemList;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonIgnore
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getGrandTotal() {
+        return GrandTotal;
+    }
+
+    public void setGrandTotal(BigDecimal grandTotal) {
+        GrandTotal = grandTotal;
+    }
+
+    public List<CartItem> getCartItemList() {
+        return cartItemList;
+    }
+
+    public void setCartItemList(List<CartItem> cartItemList) {
+        this.cartItemList = cartItemList;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
