3061a4977cf0a3a8804f1db730ed95dedb36729c
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-19 04:50:11 +0200

    Added support for order confirmation mail

--- backend/src/main/java/com/candyland/utility/MailConstructor.java
+++ backend/src/main/java/com/candyland/utility/MailConstructor.java
@@ -1,17 +1,29 @@
 package com.candyland.utility;
 
+import com.candyland.domain.Order;
 import com.candyland.domain.User;
+
+import java.util.Locale;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Component
 public class MailConstructor {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private TemplateEngine templateEngine;
 
     public SimpleMailMessage constructNewUserEmail(User user, String password) {
         String message = "\nPlease use the following credentials to log in "
@@ -24,5 +36,26 @@ public class MailConstructor {
         email.setText(message);
         email.setFrom(env.getProperty("support.email"));
         return email;
+    }
+
+    public MimeMessagePreparator constructOrderConfirmationEmail(
+        User user, Order order, Locale locale) {
+        Context context = new Context();
+        context.setVariable("order", order);
+        context.setVariable("user", user);
+        context.setVariable("cartItemList", order.getCartItemList());
+        String text = templateEngine.process("orderConfirmationEmailTemplate", context);
+
+        MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper email = new MimeMessageHelper(mimeMessage);
+                email.setTo(user.getEmail());
+                email.setSubject("Candyland - Order Confirmation " + order.getId());
+                email.setText(text, true);
+                email.setFrom(new InternetAddress("teamcandyland6@gmail.com"));
+            }
+        };
+        return messagePreparator;
     }
 }
