e79c946d40e0e6f6a862b53950d2a84f20e6236b
Klaus Sinani <klauscfhq@protonmail.com>
2018-03-19 04:57:06 +0200

    Added billing-address entity CRUD repo

---
+++ backend/src/main/java/com/candyland/repository/BillingAddressRepository.java
@@ -0,0 +1,9 @@
+package com.candyland.repository;
+
+import com.candyland.domain.BillingAddress;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BillingAddressRepository extends CrudRepository<BillingAddress, Long> {
+
+}
